317a04f0c29716baf3656427871afda7839567cd
Atul Singh <atulsolanki30@gmail.com>
2026-08-31 06:54:25 +0530

    added some examples

--- basics/function-closures/function-closures.go
+++ basics/function-closures/function-closures.go
@@ -1,12 +1,37 @@
 package main 
 
 
+import "fmt"
+
+func makeCounter() func() int {
+
+	count :=0
+
+	return func() int{
+
+		count++
+
+		return count
+	}
+
+}
 
 
 func main (){
 
 
-	
+// Intitialise function closures
+
+c :=makeCounter()
+
+// calling closures
+fmt.Println("First Call to counter: ", c())
+fmt.Println("Second Call to counter: ", c())
+fmt.Println("Third Call to counter: ", c())
+fmt.Println("Fourth Call to counter: ", c())
+fmt.Println("Fifth Call to counter: ", c())
+
+
 }
 
 
